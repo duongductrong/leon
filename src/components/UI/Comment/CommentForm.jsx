aed55df8faacf341b73f89916bb775807caeb01e
Duong Duc Trong <duongductrong06@Gmail.com>
2020-03-21 15:06:45 +0700

    rename className component and rename file css

--- src/components/UI/Comment/CommentForm.jsx
+++ src/components/UI/Comment/CommentForm.jsx
@@ -10,22 +10,22 @@ class CommentForm extends React.Component {
         const { onSubmit, style, className } = this.props;
         const { title } = this.props;
         return (
-            <form onSubmit={onSubmit} style={style} className={`comments${` ${className}`}`}>
+            <form onSubmit={onSubmit} style={style} className={`comment-form${` ${className}`}`}>
                 <WidgetTitle center> {title} </WidgetTitle>
-                <div className="comments__group">
+                <div className="comment-form__group">
                     <Label htmlFor="comment"> <small>Comment*</small> </Label>
                     <Textarea id="comment"></Textarea>
                 </div>
-                <div className="comments__multiple-group comments--grid3">
-                    <div className="comments__group">
+                <div className="comment-form__multiple-group comment-form--grid3">
+                    <div className="comment-form__group">
                         <Label htmlFor="name"> <small>Name*</small> </Label>
                         <Input id="name" value="" />
                     </div>
-                    <div className="comments__group">
+                    <div className="comment-form__group">
                         <Label htmlFor="email"> <small>Email*</small> </Label>
                         <Input id="email" value="" />
                     </div>
-                    <div className="comments__group">
+                    <div className="comment-form__group">
                         <Label htmlFor="website"> <small>Website</small> </Label>
                         <Input id="website" value="" />
                     </div>
