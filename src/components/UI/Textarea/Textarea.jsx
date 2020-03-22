0f908a40cacffffdcc0b8ef27e4e9701ca711128
Duong Duc Trong <duongductrong06@Gmail.com>
2020-03-22 13:43:00 +0700

    add attribute name, onChange

--- src/components/UI/Textarea/Textarea.jsx
+++ src/components/UI/Textarea/Textarea.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Textarea extends React.Component {
     render() {
-        const { name, id, cols, rows, className } = this.props;
+        const { name, id, cols, rows, className, value, onChange } = this.props;
         const { children } = this.props;
         return (
-            <textarea className={`textarea${`${className ? " " + className : ""}`}`} name={name} id={id} cols={cols} rows={rows} value={children}></textarea>
+            <textarea className={`textarea${`${className ? " " + className : ""}`}`} onChange={onChange} name={name} value={value} id={id} cols={cols} rows={rows} value={children}></textarea>
         )
     }
 }
