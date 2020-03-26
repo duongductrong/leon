6fc5fb9fde15f13181281c381ff2e47c5c93d0d9
Duong Duc Trong <duongductrong06@Gmail.com>
2020-03-26 23:54:57 +0700

    Add content attribute props

--- src/components/UI/FollowMe/FollowMe.jsx
+++ src/components/UI/FollowMe/FollowMe.jsx
@@ -5,26 +5,35 @@ import { faFacebookF, faTwitter, faInstagram, faLinkedin } from '@fortawesome/fr
 
 class FollowMe extends React.Component {
     render() {
-        const { title, facebook, instagram, twitter, linkedin } = this.props;
+        const { title, facebook, instagram, twitter, linkedin, content } = this.props;
         return (
             <div className="follow-me">
                 <WidgetTitle left> { title } </WidgetTitle>
                 <div className="follow-me__community">
-                    <a className="follow-me__community__facebook" href={facebook} target="_blank">
+                    <a className="follow-me__community__facebook" href={content["facebook"]} target="_blank">
                         <div><FontAwesomeIcon icon={faFacebookF} /></div>
                     </a>
-                    <a className="follow-me__community__twitter" href={twitter} target="_blank">
+                    <a className="follow-me__community__twitter" href={content["twitter"]} target="_blank">
                         <div><FontAwesomeIcon icon={faTwitter} /></div>
                     </a>
-                    <a className="follow-me__community__ins" href={instagram} target="_blank">
+                    <a className="follow-me__community__ins" href={content["instagram"]} target="_blank">
                         <div><FontAwesomeIcon icon={faInstagram} /></div>
                     </a>
-                    <a className="follow-me__community__linkedin" href={linkedin} target="_blank">
+                    <a className="follow-me__community__linkedin" href={content["linkedin"]} target="_blank">
                         <div><FontAwesomeIcon icon={faLinkedin} /></div>
                     </a>
                 </div>
             </div>
         )
+    }
+}
+
+FollowMe.defaultProps = {
+    content: {
+        facebook: "/",
+        twitter: "/",
+        linkedin: "/",
+        instagram: "/"
     }
 }
 
